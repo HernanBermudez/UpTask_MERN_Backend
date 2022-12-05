9a425af8c687b47d991a951851663324ecbcff04
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-12-05 02:15:07 -0600

    edit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ conectarDB();
 
 // Configuracion de CORS
 // const whiteList = [process.env.FRONTEND_URL]
-const vercelFront = ['https://taskaminhb-frontend.vercel.app/']
+const vercelFront = ['taskaminhb-frontend.vercel.app']
 
 const corsOptions = {
     origin: function (origin, callback) {
